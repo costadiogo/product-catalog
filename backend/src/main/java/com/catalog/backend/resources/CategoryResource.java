74d7ab7756b67b76e812dc46b17c6f20f2f98166
Diogo Costa <costadiogo30@gmail.com>
2022-08-25 06:32:38 -0300

    Create Category

--- backend/src/main/java/com/catalog/backend/resources/CategoryResource.java
+++ backend/src/main/java/com/catalog/backend/resources/CategoryResource.java
@@ -2,13 +2,17 @@ package com.catalog.backend.resources;
 
 import com.catalog.backend.dto.CategoryDTO;
 import com.catalog.backend.service.CategoryService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/v1/categories")
@@ -31,5 +35,15 @@ public class CategoryResource {
         CategoryDTO listCategoryById = service.findById(id);
 
         return ResponseEntity.ok().body(listCategoryById);
+    }
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO category) {
+
+        category = service.create(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(category);
     }
 }
